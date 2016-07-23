a1b1bee438113803830c9fa3f5aea4d82e265a9b
anush24 <agovindan89@gmail.com>
2016-07-23 11:36:56 -0700

    Initial Setup-GoodEats

---
+++ goodeats/app/src/main/java/com/appetite/Login.java
@@ -0,0 +1,138 @@
+package com.appetite;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import java.io.InputStream;
+
+
+public class Login extends AppCompatActivity {
+
+    public GoogleApiClient mGoogleApiClient;
+    public static int RC_SIGN_IN = 1;
+    TextView mStatusTextView;
+    Button bLogin;
+    EditText etUserName, etPassword;
+    TextView tvRegisterLink;
+    String name,address,username,password;
+    int id,phone_no;
+    String userPwd;
+    String email;
+    Uri photoUrl;
+    public static String userName;
+    Firebase chefRef,customerRef;
+    String uname;
+    SignInButton signInButton;
+    public static boolean googleSignIn = false;
+    int count;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Firebase.setAndroidContext(this);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setColorScheme(SignInButton.COLOR_AUTO);
+        signInButton.setScopes(gso.getScopeArray());
+
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                signIn();
+            }
+        });
+
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("SIGN IN", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            googleSignIn = true;
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            userName = acct.getDisplayName();
+            email = acct.getEmail();
+            photoUrl = acct.getPhotoUrl();
+
+            displayMenu();
+
+        } else {
+                Toast.makeText(getApplicationContext(),"Invalid Login",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void displayMenu(){
+        Intent menuList = new Intent(this, MenuList.class);
+        startActivity(menuList);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+
+}
---
+++ goodeats/app/src/main/java/com/appetite/MenuList.java
@@ -0,0 +1,215 @@
+package com.appetite;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kavi on 2/13/16.
+ */
+public class MenuList extends AppCompatActivity {
+
+    TextView welcometxt;
+    TextView item_name, item_price;
+    Firebase mRef,menuRef,mRef1,mRef2;
+    ImageView image;
+    String user,status;
+    ListView menulistView;
+    List<Menu> menuList;
+    ImageButton searchBtn;
+    String searchDish;
+    EditText searchWord;
+    MenulistAdapter menuListAdapter;
+    String chef,dish,quan,cust;
+    static boolean orderPage=false;
+    Login signOut;
+    public GoogleApiClient mGoogleApiClient;
+    String itemName,itemPrice,itemImage,itemCuisine;
+    int count=0;
+    int counts=0;
+    Menu menuItems;
+    int dataReading = 0;
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.menulist_layout);
+        Firebase.setAndroidContext(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        searchBtn =(ImageButton) (findViewById(R.id.searchB));
+        searchWord = (EditText)(findViewById(R.id.searchW));
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String keyWord = searchWord.getText().toString();
+                searchContent(keyWord);
+                Log.d("QWERTY", keyWord);
+            }
+        });
+
+
+        mRef = new Firebase("https://app-etite.firebaseio.com/menulist");
+        menuList = new ArrayList<Menu>();
+        mRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                menuList.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+
+
+                    if (postSnapshot.child("item_name").getValue() != null && postSnapshot.child("imageEncoded") != null && postSnapshot.child("cuisine") != null) {
+                        itemName = postSnapshot.child("item_name").getValue().toString();
+                        itemImage = postSnapshot.child("imageEncoded").getValue().toString();
+                        itemCuisine = postSnapshot.child("cuisine").getValue().toString();
+                        menuList.add(new Menu(itemName, itemImage, itemCuisine));
+                    }
+
+                }
+                menuList();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.d("The read failed: ", firebaseError.getMessage());
+
+            }
+        });
+
+
+    }
+
+    public void menuList(){
+
+        menulistView = (ListView) findViewById(R.id.listView);
+
+        menuListAdapter = new MenulistAdapter(this, R.layout.menulist_rowlayout, menuList);
+        menulistView.setAdapter(menuListAdapter);
+
+        menulistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                menuItems = menuList.get(position);
+                Intent dishInfo = new Intent(getApplicationContext(),DishInfo.class);
+                startActivity(dishInfo);
+            }
+        });
+    }
+
+
+    public void searchContent(String word){
+        searchDish=word;
+        menuList.clear();
+        mRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    if (postSnapshot.child("item_name").getValue() != null && postSnapshot.child("imageEncoded") != null && postSnapshot.child("cuisine") != null) {
+                        String itemName = postSnapshot.child("item_name").getValue().toString();
+                        String itemImage = postSnapshot.child("imageEncoded").getValue().toString();
+                        String itemCuisine = postSnapshot.child("cuisine").getValue().toString();
+                        if (itemName.equalsIgnoreCase(searchDish) || itemCuisine.equalsIgnoreCase(searchDish)) {
+                            menuList.add(new Menu(itemName, itemImage, itemCuisine));
+                        }
+                    }
+                    menuList();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.d("The read failed: ", firebaseError.getMessage());
+            }
+        });
+    }
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        // Specify the calling package to identify your application
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+        // Display an hint to the user about what he should say.
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hungry for?!");
+        // Given an hint to the recognizer about what the user is going to say
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Specify how many results you want to receive. The results will be sorted
+        // where the first result is the one with higher confidence.
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> matches = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+
+            searchWord.setText(matches.get(0));
+            String voiceInput = searchWord.getText().toString();
+            if(voiceInput!=null ){
+                searchContent(voiceInput);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        if (MainActivity.cookModule){
+        getMenuInflater().inflate(R.menu.menu_cook, menu);}
+        else{
+            getMenuInflater().inflate(R.menu.menu_eat, menu);
+        }
+        getMenuInflater().inflate(R.menu.menu_mic, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
